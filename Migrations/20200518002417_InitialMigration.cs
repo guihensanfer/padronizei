496b87df761db2e1a3a5c00b348548b3954492a4
Guilherme Ferreira <guihensanfer@gmail.com>
2020-05-17 21:29:35 -0300

    Configurado banco de dados com modelo de dados, criado tabelas com mmigrations do Core.

---
+++ Migrations/20200518002417_InitialMigration.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Padronizei.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Colaborador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 200, nullable: false),
+                    Bio = table.Column<string>(nullable: true),
+                    Matricula = table.Column<string>(maxLength: 100, nullable: true),
+                    Email = table.Column<string>(maxLength: 200, nullable: false),
+                    DataCriacao = table.Column<DateTime>(nullable: false),
+                    DepartamentoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaborador", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conteudo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(maxLength: 150, nullable: false),
+                    Corpo = table.Column<string>(nullable: false),
+                    Visibilidade = table.Column<int>(nullable: false),
+                    DataCriacao = table.Column<DateTime>(nullable: false),
+                    ColaboradorId = table.Column<int>(nullable: false),
+                    DepartamentoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conteudo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    OrganizacaoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GrupoTrabalho",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 80, nullable: false),
+                    Descricao = table.Column<string>(maxLength: 200, nullable: false),
+                    DataCriacao = table.Column<DateTime>(nullable: false),
+                    OrganizacaoId = table.Column<int>(nullable: false),
+                    ColaboradorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GrupoTrabalho", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organizacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 80, nullable: false),
+                    CNPJ = table.Column<string>(maxLength: 15, nullable: true),
+                    DataCriacao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizacao", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colaborador");
+
+            migrationBuilder.DropTable(
+                name: "Conteudo");
+
+            migrationBuilder.DropTable(
+                name: "Departamento");
+
+            migrationBuilder.DropTable(
+                name: "GrupoTrabalho");
+
+            migrationBuilder.DropTable(
+                name: "Organizacao");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Padronizei.Models;
 
 namespace Padronizei
 {
@@ -24,6 +26,9 @@ namespace Padronizei
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AplicacaoDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
